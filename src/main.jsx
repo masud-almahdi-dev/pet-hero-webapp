497db058e77342ba29c89875585850ca8ca893ac
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-11-24 21:21:31 +0600

    introduced tailwind to project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import { RouterProvider} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+const petrouter = createBrowserRouter([{
+    path: "/",
+    element: <h1 className='bg-red-300'>Hello!</h1>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={petrouter}>
+    </RouterProvider>
   </React.StrictMode>,
 )
