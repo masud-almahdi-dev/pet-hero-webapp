0f00d4dac4c1c70b3df3e6dea3bd1f8e3feed29b
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-11-29 11:50:07 +0600

    created details pages

--- src/pages/donationdetails.jsx
+++ src/pages/donationdetails.jsx
@@ -1,20 +1,20 @@
 const DonationDetails = () => {
-    return ( 
+    return (
         <div>
-            
+
             <div>
-                <img src="/cat1.jpg" alt="" />
-                <h3>Pet Name</h3>
-                <h4>Maximum Donation Amount</h4>
-                <h4>Donated Amount</h4>
-                <h4>Submitted Date</h4>
-                <button>View Details</button> 
-            </div>
-            <div>
-                <button>DONATE NOW</button>
+                <h2 className="w-full text-center p-4 font-semibold">DONATION DETAILS</h2>
+                <img src="/cat1.jpg" className="object-cover w-full md:w-1/2 mx-auto" alt="" />
+                <div className="p-4 flex flex-col gap-4">
+                    <h3 className="text-xl">Pet Name</h3>
+                    <h4>Maximum Donation Amount</h4>
+                    <h4 className="text-sm italic font-semibold">Donated Amount</h4>
+                    <h4 className="text-sm italic">Submitted Date</h4>
+                    <button className="mx-auto w-max bg-yellow-200 text-black py-2 px-4 rounded-sm hover:bg-yellow-100">DONATE NOW</button>
+                </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default DonationDetails;
--- src/pages/petdetails.jsx
+++ src/pages/petdetails.jsx
@@ -2,15 +2,15 @@ const PetDetails = () => {
     return (
         <div>
             <div>
-                <img src="/cat1.jpg" alt="" />
-                <h3>Pet Name</h3>
-                <h4>Maximum Donation Amount</h4>
-                <h4>Donated Amount</h4>
-                <h4>Submitted Date</h4>
-                <button>View Details</button>
-            </div>
-            <div>
-                <button>DONATE NOW</button>
+                <h2 className="w-full text-center p-4 font-semibold">DETAILS</h2>
+                <img src="/cat1.jpg" className="object-cover w-full md:w-1/2 mx-auto" alt="" />
+                <div className="p-4 flex flex-col gap-4">
+                    <h3 className="text-xl">Pet Name</h3>
+                    <h4>Age</h4>
+                    <h4 className="text-sm italic font-semibold">Location</h4>
+                    <h4 className="text-sm italic">Submitted Date</h4>
+                    <button className="mx-auto w-max bg-yellow-200 text-black py-2 px-4 rounded-sm hover:bg-yellow-100">ADOPT NOW</button>
+                </div>
             </div>
         </div>
     );
